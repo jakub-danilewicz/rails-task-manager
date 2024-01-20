20570dec575f4ece93954d4ff335040a03736bc6
Jakub Danilewicz <jakubdanilewicz@gmail.com>
2024-01-20 16:32:27 +0100

    1.1

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new # Needed to instantiate the form_with
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    @task.save
+    # No need for app/views/restaurants/create.html.erb
+    redirect_to tasks_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(tasks_params)
+    # No need for app/views/restaurants/update.html.erb
+    redirect_to tasks_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    # No need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Deleting Tasks..."
+Task.delete_all
+sleep 5
+
+task_1 = Task.create(title: "Laundry", details: "Do not mix colors!")
+puts "#{task_1.title} Task with id: ##{task_1.id} was created"
+
+task_2 = Task.create(title: "Studying", details: "A lot of flashcards to do", completed: true)
+puts "#{task_2.title} Task with id: ##{task_2.id} was created"
